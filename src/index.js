c69379a1b9cbddaf7b4080d697ea5acd8d4ad825
@ameahmada04 <ameahmada04@gmail.com>
2025-02-20 09:30:13 +0000

    Deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -2,24 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { HashRouter as Route } from "react-router-dom";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <Router basename="/portfolio-ahmada">
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
+
+// ReactDOM.render(
+//   <Router basename="/portfolio-ahmada">
 //     <App />
-//   </React.StrictMode>
+//   </Router>,
+//   document.getElementById("root")
 // );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
